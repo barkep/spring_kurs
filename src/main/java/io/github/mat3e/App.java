7b8c5ba109476589a7833b7b7b7dc6c857900116
Bartek <bartekkepka6@gmail.com>
2019-09-19 22:07:12 +0200

    init

---
+++ src/main/java/io/github/mat3e/App.java
@@ -0,0 +1,38 @@
+package io.github.mat3e;
+
+import org.eclipse.jetty.annotations.AnnotationConfiguration;
+import org.eclipse.jetty.plus.webapp.EnvConfiguration;
+import org.eclipse.jetty.plus.webapp.PlusConfiguration;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.Configuration;
+import org.eclipse.jetty.webapp.FragmentConfiguration;
+import org.eclipse.jetty.webapp.JettyWebXmlConfiguration;
+import org.eclipse.jetty.webapp.MetaInfConfiguration;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.webapp.WebInfConfiguration;
+import org.eclipse.jetty.webapp.WebXmlConfiguration;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        var webapp = new WebAppContext();
+        webapp.setResourceBase("src/main/webapp");
+        webapp.setContextPath("/");
+        webapp.setConfigurations(new Configuration[]{
+                new AnnotationConfiguration(),
+                new WebInfConfiguration(),
+                new WebXmlConfiguration(),
+                new MetaInfConfiguration(),
+                new FragmentConfiguration(),
+                new EnvConfiguration(),
+                new PlusConfiguration(),
+                new JettyWebXmlConfiguration()
+        });
+        webapp.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*/classes/.*");
+//        webapp.addServlet(HelloServlet.class, "/api/*");
+        var server = new Server(8081);
+        server.setHandler(webapp);
+
+        server.start();
+        server.join();
+    }
+}
