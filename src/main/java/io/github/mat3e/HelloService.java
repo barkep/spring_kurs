f2e7db10a5ba059c650de72eb2ee3a0bbf3de0ef
Bartek <bartekkepka6@gmail.com>
2019-09-19 23:51:28 +0200

    40 połączenie serwis-servlet

--- src/main/java/io/github/mat3e/HelloService.java
+++ src/main/java/io/github/mat3e/HelloService.java
@@ -4,8 +4,27 @@ import java.util.Optional;
 
 class HelloService {
     static final String FALLBACK_NAME = "world";
+    static final Lang FALLBACK_LANG = new Lang(1L, "Hello", "en");
+
+    private LangRepository repository;
+
+    HelloService() {
+        this(new LangRepository());
+    }
+
+    private HelloService(LangRepository repository) {
+        this.repository = repository;
+    }
+
 
     String prepareGreeting(String name) {
-        return "Hello " + Optional.ofNullable(name).orElse(FALLBACK_NAME) + " !";
+        return prepareGreeting(name, null);
+    }
+
+    String prepareGreeting(String name, String lang) {
+        var langId = Optional.ofNullable(lang).map(Long::valueOf).orElse(FALLBACK_LANG.getId());
+        var welcomeMsg = repository.findByID(langId).orElse(FALLBACK_LANG).getWelcomeMsg();
+        String nameToWelcome = Optional.ofNullable(name).orElse(FALLBACK_NAME);
+        return welcomeMsg + " " + nameToWelcome + " !";
     }
 }
